b6bcc3964a88e4ef4b65569ca2021987944892dc
Aleksey Savin <saleksey67@gmail.com>
2017-10-31 18:38:34 +0300

    added fixes

--- Variables.R
+++ Variables.R
@@ -1,4 +1,4 @@
-my_vector1 <- c(21,18,21,19,25,20,17,17,18,22)
+my_vector1 <- c(21,18,21,19,25,20,17,17,18,22, 99)
 mean_num <- mean(my_vector1)
 sd_num = sd(my_vector1)
 my_vector4 <- c(my_vector1 > mean_num +- sd_num ) || c(my_vector1 < mean_num +- sd_num )
@@ -14,3 +14,11 @@ data[[3]][2]
 df <- data.frame(Name = name, Age = age, Status = is_maried)
 
 typeof(df)
+
+#1.2
+my_vector1 <- c(0, 16, 21,19,25,20,17,18,29, 50)
+mean_num <- mean(my_vector1)
+sd_num = sd(my_vector1)
+my_left <- c(my_vector1 - sd_num)
+my_right <- c(my_vector1 + sd_num)
+my_vector1[my_vector1 > my_left]
