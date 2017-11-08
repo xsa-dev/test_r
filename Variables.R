b11f0610f17726374594a6b1933c013f1f90b58f
Aleksey Savin <saleksey67@gmail.com>
2017-11-08 09:52:32 +0300

    1.2 done

--- Variables.R
+++ Variables.R
@@ -16,9 +16,9 @@ df <- data.frame(Name = name, Age = age, Status = is_maried)
 typeof(df)
 
 #1.2
-my_vector1 <- c(0, 16, 21,19,25,20,17,18,29, 50)
-mean_num <- mean(my_vector1)
-sd_num = sd(my_vector1)
-my_left <- c(my_vector1 - sd_num)
-my_right <- c(my_vector1 + sd_num)
-my_vector1[my_vector1 > my_left]
+my_vector <- c(21,18,21,19,25,20,17,17, 18,22,17,18,18,19,19,27,21,20,24,17,15,24,24,29,19,14,21,17,19,18,18,20,21,21,19,19,17,21,13,17,13,23,15,23,24,16,17,25,24,22)
+mean_num <- mean(my_vector)
+sd_num = sd(my_vector)
+my_left <- c(mean_num - sd_num)
+my_right <- c(mean_num + sd_num)
+my_vector_2 <- my_vector[my_vector >= my_left & my_vector <= my_right]
